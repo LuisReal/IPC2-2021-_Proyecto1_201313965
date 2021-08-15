b207f4cfa967f724e06d379d1bbcbe106f7f1062
LuisReal <74241860+LuisReal@users.noreply.github.com>
2021-08-15 00:13:55 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,7 +1,35 @@
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
 
 class Carga:
     def __init__(self):
         self.doc = ""
+        self.fila = 0
+        self.columna = 0
+        self.matriz = []
+        self.diccionario = {}
+   
     def archivo(self, ruta):
-        self.doc = minidom.parse(ruta) #se ingresa la ruta completa del archivo para que funcione
+    
+        xmlfile = ruta
+
+        doc = ET.parse(xmlfile)
+        root = doc.getroot()
+
+        #print(root[0][3].attrib)
+
+        for position in root.iter('posicion'): 
+            print(position.attrib)
+             
+
+        for matriz_prueba in root.findall('terreno'): # terreno es el nombre de la etiqueta del archivo robot.xml
+            nombre = matriz_prueba.get('nombre') # nombre es la etiqueta adentro de la etiqueta terreno
+            print(nombre)
+''' 
+            self.fila = int(matriz_prueba.get('n'))
+            self.columna = matriz_prueba.get('m')
+
+            print("\nel nombre de la matriz es: ", nombre, "\nel numero de filas es: ", self.fila,
+            "\nel numero de columnas es: ", self.columna)
+            print("el tamano de la matriz es: ", len(matriz_prueba),"\n") 
+'''
